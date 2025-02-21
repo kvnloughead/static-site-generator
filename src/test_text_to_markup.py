9b737eba47d90a19c88fd46e880b543f1cbcda8b
Kevin Loughead <kvnloughead@gmail.com>
2025-02-20 20:43:14 -0500

    feat: add split_nodes_by_delimiter function
     - This function is for parsing markdown strings containing
     - **bold**, *italic*, or `code` formatted text.

--- src/test_text_to_markup.py
+++ src/test_text_to_markup.py
@@ -1,7 +1,13 @@
 import unittest
-from text_to_markup import text_to_html
+from text_to_markup import text_to_html, split_node, split_nodes_by_delimiter
 from textnode import TextNode, TextType
 from leafnode import LeafNode
+
+delimiters = [
+    {"char": "**", "text_type":  TextType.BOLD},
+    {"char": "*", "text_type": TextType.ITALIC},
+    {"char": "`", "text_type": TextType.CODE}
+]
 
 class TestTextToMarkup(unittest.TestCase):
     simple_cases = [
@@ -111,3 +117,175 @@ class TestTextToMarkup(unittest.TestCase):
                 
                 leaf_from_text = text_to_html(text_node)
                 self.assertEqual(leaf_node, leaf_from_text)
+
+class TestSplitNode(unittest.TestCase):
+    no_delimiter_cases = [
+        {
+            "name": "Without delimiter",
+            "delimiter": "*",
+            "text_type": TextType.BOLD,
+            "node": TextNode("I am not bold", TextType.NORMAL),
+            "expected": [
+                TextNode("I am not bold", TextType.NORMAL),
+
+            ],
+        },                        
+    ]
+
+    one_delimiter_pair_cases = [
+        {
+            "name": "normal",
+            "left": "ABC",
+            "middle": "DEF",
+            "right": "XYZ",
+            "result_length": 3
+        },
+        {
+            "name": "preserves whitespace",
+            "left": " ABC ",
+            "middle": " DEF ",
+            "right": " XYZ ",
+            "result_length": 3
+        },
+        {
+            "name": "initial delimiter omits left",
+            "left": "",
+            "middle": "DEF",
+            "right": "XYZ",
+            "result_length": 2,
+        },
+        {
+            "name": "empty left and right returns middle",
+            "left": "",
+            "middle": "DEF",
+            "right": "",
+            "result_length": 1
+        },
+        {
+            "name": "empty middle returns all",
+            "left": "ABC",
+            "middle": "",
+            "right": "XYZ",
+            "result_length": 3 
+        },
+        {
+            "name": "initial and final whitespace is preserved",
+            "left": "  ",
+            "middle": "DEF",
+            "right": "  ",
+            "result_length": 3 
+        },
+        {
+            "name": "all empty returns only the middle node",
+            "left": "",
+            "middle": "",
+            "right": "",
+            "result_length": 1
+        },
+        {
+            "name": "all whitespace returns three nodes",
+            "left": " ",
+            "middle": " ",
+            "right": " ",
+            "result_length": 3
+        },
+    ]                              
+
+    def test_no_delimiter_cases(self):
+        for case in self.no_delimiter_cases:
+            with self.subTest(case["name"]):
+                result = split_node(case["node"], case["delimiter"], case["text_type"])
+                self.assertEqual(result, case["expected"])
+
+    def test_empty_node(self):
+        node = TextNode("", TextType.NORMAL)
+        self.assertEqual(split_node(node, "*", TextType.BOLD), [])
+
+    def test_with_one_delimiter_pair(self):
+        for case in self.one_delimiter_pair_cases:
+            with self.subTest(case["name"]):
+                for delimiter in delimiters:
+                    left, mid, right = case["left"], case["middle"], case["right"]
+                    d, type = delimiter["char"], delimiter["text_type"]
+                    text = left + d + mid + d + right
+                    node = TextNode(text, TextType.NORMAL)
+                    actual = split_node(node, d, type)
+                    
+                    expected = []
+                    if left != "":
+                        expected.append(TextNode(left, TextType.NORMAL))
+                    expected.append(TextNode(mid, type))
+                    if right != "":
+                        expected.append(TextNode(right, TextType.NORMAL))
+
+                    self.assertEqual(actual, expected)
+                    if "result_length" in case:
+                        self.assertEqual(len(actual), case["result_length"])
+
+    def test_missing_closing_delimiter(self):
+        with self.assertRaises(Exception) as context:
+            node = TextNode("This *is invalid markdown", TextType.NORMAL)
+            split_node(node, "*", TextType.BOLD)
+        self.assertTrue('Invalid markdown. No closing delimiter found, searching for "*".')
+
+class TestSplitNodeByDelimiter(unittest.TestCase):
+    simple_cases = [
+        {
+            "name": "simple case with match",
+            "delimiter": "`",
+            "text_type": TextType.CODE,
+            "old_nodes": [
+                LeafNode("p", "text"), TextNode("Text with a `code block` word", TextType.NORMAL), LeafNode("p", "text")
+            ],
+            "expected": [
+                LeafNode("p", "text"),  
+                TextNode("Text with a ", TextType.NORMAL),
+                TextNode("code block", TextType.CODE),
+                TextNode(" word", TextType.NORMAL), 
+                LeafNode("p", "text")
+            ]
+        },
+        {
+            "name": "simple case without match",
+            "delimiter": "*",
+            "text_type": TextType.ITALIC,
+            "old_nodes": [
+                LeafNode("p", "text"), TextNode("Text with a `code block` word", TextType.NORMAL), LeafNode("p", "text")
+            ],
+            "expected": [
+                LeafNode("p", "text"), TextNode("Text with a `code block` word", TextType.NORMAL), LeafNode("p", "text")
+            ],
+        },
+        {
+            "name": "multiple text nodes case",
+            "delimiter": "**",
+            "text_type": TextType.BOLD,
+            "old_nodes": [
+                 TextNode("Text with a **bold** word", TextType.NORMAL), TextNode("Normal text", TextType.NORMAL), TextNode("*all italic*", TextType.NORMAL)
+            ],
+            "expected": [TextNode("Text with a ", TextType.NORMAL),
+                         TextNode("bold", TextType.BOLD),
+                         TextNode(" word", TextType.NORMAL),
+                         TextNode("Normal text", TextType.NORMAL),
+                         TextNode("*all italic*", TextType.NORMAL),
+            ]
+        }
+    ]
+
+    def test_simple_cases(self):
+        for case in self.simple_cases:
+            with self.subTest(case["name"]):
+                new_nodes = split_nodes_by_delimiter(
+                    case["old_nodes"],
+                    case["delimiter"],
+                    case["text_type"])
+                self.assertEqual(new_nodes, case["expected"])
+
+    def test_missing_closing_delimiter(self):
+        with self.assertRaises(Exception) as context:
+            node = TextNode("This *is invalid markdown", TextType.NORMAL)
+            split_nodes_by_delimiter(
+                [TextNode("valid", TextType.NORMAL), node],
+                "*",
+                TextType.BOLD)
+        self.assertTrue('Invalid markdown. No closing delimiter found, searching for "*".')
--- src/text_to_markup.py
+++ src/text_to_markup.py
@@ -26,8 +26,8 @@ def split_nodes_by_delimiter(old_nodes, delimiter, text_type, new_nodes=None):
     on the supplied delimiter, turns them into TextNodes, and returns a new list of nodes. An example will be worth all these words. Give this:
 
     ```python
-    node = LeafNode("p")
-    text_node = TextNode("This is text with a `code block` word", TextType.TEXT)
+    node = LeafNode("p", "text")
+    text_node = TextNode("Text with a `code block` word", TextType.NORMAL)
     new_nodes = split_nodes_delimiter([text_node, node], "`", TextType.CODE)
     ```
 
@@ -35,21 +35,23 @@ def split_nodes_by_delimiter(old_nodes, delimiter, text_type, new_nodes=None):
 
     ```python
     [
-        TextNode("This is text with a ", TextType.TEXT),
+        TextNode("This with a ", TextType.NORMAL),
         TextNode("code block", TextType.CODE),
-        TextNode(" word", TextType.TEXT),
-        LeafNode("p")
+        TextNode(" word", TextType.NORMAL),
+        LeafNode("p", "text")
     ]
     ```
     """
-    if len(result) == 0:
-        return result
+    result = []
+    if len(old_nodes) == 0 or (new_nodes != None and len(new_nodes) == 0):
+        return new_nodes
+    elif new_nodes:
+        result = new_nodes.copy()
     
-    result = new_nodes.copy()
     current_node = old_nodes[0]
 
     # Add non-text nodes and text nodes with no value to the new list unchanged.
-    if not isinstance(current_node, TextNode) or not current_node.value:
+    if not isinstance(current_node, TextNode) or not current_node.text:
         result.append(current_node)
     else:
         split_nodes = split_node(current_node, delimiter, text_type)
@@ -59,8 +61,23 @@ def split_nodes_by_delimiter(old_nodes, delimiter, text_type, new_nodes=None):
 
 def split_node(text_node, delimiter, text_type):
     """
-    split_node splits the given text node on the given delimiter. See 
-    split_nodes_by_delimiter for examples.
+    split_node splits the given text node on the given delimiter. The delimiter
+    can be one or more characters and are omitted from the result node's text. Guidelines followed:
+
+    - Whitespace is always preserved, even if that's all that's in text segment
+    - If the leftmost or rightmost segments are outside of a delimiter and empty, they are omitted
+    - Empty pairs of delimiters are NOT omitted
+    - If there is an unclosed delimiter, an exception is raised
+
+     For example, calling `split_node(TextNode("ABC*DEF*XYZ), "*", TextType.ITALIC)` will return this:
+
+    ```python
+    [
+        TextNode("ABC", TextType.NORMAL),
+        TextNode("DEF", TextType.ITALIC)
+        TextNode("XYZ", TextType.NORMAL)
+    ]
+    ```
     """
     if len(text_node.text) == 0:
         return []
@@ -69,7 +86,14 @@ def split_node(text_node, delimiter, text_type):
     result_nodes = []
     curr_node = TextNode("", TextType.NORMAL) # default to normal
 
-    for i, char in enumerate(text_node.text):
+    i = 0
+    if text_node.text[:len(delimiter)] == delimiter:
+        i += len(delimiter)
+        open_delimiter = True
+        curr_node.text_type = text_type
+
+    while i < len(text_node.text):
+        char = text_node.text[i]
         # Opening delimiter found
         if text_node.text[i:i+len(delimiter)] == delimiter and not open_delimiter:
             # Add the current node to the results if it isn't empty
@@ -77,22 +101,23 @@ def split_node(text_node, delimiter, text_type):
                 result_nodes.append(curr_node)
             # Initialize the new current node with the new text type
             curr_node = TextNode("", text_type)
+            i += len(delimiter) - 1
             open_delimiter = True
         
         # Closing delimiter found
         elif text_node.text[i:i+len(delimiter)] == delimiter:
             # Add the current node to the results if it isn't empty
-            print(i, curr_node)
-            if curr_node.text:
-                result_nodes.append(curr_node)
+            result_nodes.append(curr_node)
             # Reset open_delimiter and initialize the new current (normal) node
             open_delimiter = False
             curr_node = TextNode("", TextType.NORMAL)
+            i += len(delimiter) - 1
         else:
             curr_node.text += char
-    
+        i += 1
+
     if open_delimiter > 0:
         raise Exception(f"Invalid markdown. No closing delimiter found, searching for \"{delimiter}\".")
-    
-    result_nodes.append(curr_node)
+    if curr_node.text:
+        result_nodes.append(curr_node)
     return result_nodes

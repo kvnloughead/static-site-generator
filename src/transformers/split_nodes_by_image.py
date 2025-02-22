1c2f756e3c2bb32d31e0ae500425cf1559062122
Kevin Loughead <kvnloughead@gmail.com>
2025-02-22 16:36:15 -0500

    feat: add functions to split split nodes by images

---
+++ src/transformers/split_nodes_by_image.py
@@ -0,0 +1,62 @@
+from nodes.textnode import TextNode, TextType
+from transformers.text_to_markup import extract_markdown_images
+
+def split_nodes_by_image(old_nodes, new_nodes=None):
+    """
+    split_nodes_by_image takes a list of nodes and splits any text nodes whenever it encounters a markdown image. Here is an example:
+
+    ```python
+    text_node = TextNode("Text ![alt](image.png) text.", TextType.NORMAL)
+    node = LeafNode("p", "text")
+    new_nodes = split_nodes_delimiter([text_node, node], "`", TextType.CODE)
+    ```
+
+    The new_nodes list will look like this:
+
+    ```python
+    [
+        TextNode("Text", TextType.NORMAL),
+        TextNode("alt", TextType.IMAGE, url="image.png"),
+        TextNode(" text.", TextType.NORMAL),
+        LeafNode("p", "text")
+    ]
+    ```
+    """
+    result = []
+    if len(old_nodes) == 0 or (new_nodes != None and len(new_nodes) == 0):
+        return new_nodes or []
+    elif new_nodes:
+        result = new_nodes.copy()
+    
+    current_node = old_nodes[0]
+    if not isinstance(current_node, TextNode) or not current_node.text:
+        result.append(current_node)
+    else:
+        current_text = current_node.text
+        images = extract_markdown_images(current_text)
+        if len(images) == 0:
+            result.append(current_node)
+            return split_nodes_by_image(old_nodes[1:], result)
+        for image in images:
+            left, right = split_text_on_image(current_text, image)
+            if left != "":
+                result.append(TextNode(left, TextType.NORMAL))
+            result.append(TextNode(image[0], TextType.IMAGE, url=image[1]))
+            current_text = right
+        if current_text != "":
+            result.append(TextNode(right, TextType.NORMAL))
+    return split_nodes_by_image(old_nodes[1:], result)
+
+def split_text_on_image(text, image):
+    """
+    split_text_on_image splits a string using a markdown image as a delimiter.
+    The maxsplits parameter is set to 1, so there are only two pieces: to the 
+    left of the image and to the right. These are returned in a tuple.
+
+    - text: a string of markdown
+    - image: a tuple of two strings. The first will be used as the markdown 
+             image's alt text, the second as its URL.
+    """
+    image_string = f"![{image[0]}]({image[1]})"
+    left, right = text.split(image_string, 1)
+    return (left, right)
---
+++ src/transformers/test_split_nodes_by_image.py
@@ -0,0 +1,89 @@
+import unittest
+from transformers.split_nodes_by_image import split_nodes_by_image, split_text_on_image
+from nodes.textnode import TextNode, TextType
+from nodes.leafnode import LeafNode
+
+class TestSplitNode(unittest.TestCase):
+    simple_cases = [
+        {
+            "name": "simple case",
+            "old_nodes": [
+                LeafNode("p", "text"), TextNode("left ![alt](https://example.png) right", TextType.NORMAL), LeafNode("p", "text")
+            ],
+            "expected": [
+                LeafNode("p", "text"),
+                TextNode("left ", TextType.NORMAL),
+                TextNode("alt", TextType.IMAGE, url="https://example.png"),
+                TextNode(" right", TextType.NORMAL),
+                LeafNode("p", "text")
+            ],
+                
+        },
+        {
+            "name": "multiple images",
+            "old_nodes": [
+                LeafNode("p", "text"), TextNode("left ![alt](https://example.png) right", TextType.NORMAL), LeafNode("p", "text"), TextNode("left ![alt](https://example.png) right", TextType.NORMAL), LeafNode("p", "text"), LeafNode("p", "text")
+            ],
+            "expected": [
+                LeafNode("p", "text"),
+                TextNode("left ", TextType.NORMAL),
+                TextNode("alt", TextType.IMAGE, url="https://example.png"),
+                TextNode(" right", TextType.NORMAL),
+                LeafNode("p", "text"),
+                TextNode("left ", TextType.NORMAL),
+                TextNode("alt", TextType.IMAGE, url="https://example.png"),
+                TextNode(" right", TextType.NORMAL),
+                LeafNode("p", "text"),
+                LeafNode("p", "text")
+            ],
+                
+        },
+        {
+            "name": "empty slots",
+            "old_nodes": [
+                TextNode("left ![alt](https://example.png) right", TextType.NORMAL), TextNode("left ![alt](https://example.png) right", TextType.NORMAL)
+            ],
+            "expected": [
+                TextNode("left ", TextType.NORMAL),
+                TextNode("alt", TextType.IMAGE, url="https://example.png"),
+                TextNode(" right", TextType.NORMAL),
+                TextNode("left ", TextType.NORMAL),
+                TextNode("alt", TextType.IMAGE, url="https://example.png"),
+                TextNode(" right", TextType.NORMAL),
+            ],     
+        },
+        {
+            "name": "multiple images in text node",
+            "old_nodes": [
+                TextNode("left ![alt](https://example.png) middle ![alt](https://example.png) right", TextType.NORMAL)
+            ],
+            "expected": [
+                TextNode("left ", TextType.NORMAL),
+                TextNode("alt", TextType.IMAGE, url="https://example.png"),
+                TextNode(" middle ", TextType.NORMAL),
+                TextNode("alt", TextType.IMAGE, url="https://example.png"),
+                TextNode(" right", TextType.NORMAL),
+            ],     
+        },
+        {
+            "name": "no images in text node",
+            "old_nodes": [
+                TextNode("no images", TextType.NORMAL)
+            ],
+            "expected": [
+                TextNode("no images", TextType.NORMAL),
+            ],     
+        },
+        {
+            "name": "no nodes",
+            "old_nodes": [],
+            "expected": [],
+        }
+
+    ]
+
+    def test_simple_cases(self):
+           for case in self.simple_cases:
+            with self.subTest(case["name"]):
+                actual = split_nodes_by_image(case["old_nodes"])
+                self.assertEqual(actual, case["expected"])

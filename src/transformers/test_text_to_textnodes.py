0d48fab36bcd255c3f77e13c3dfc858613a4b785
Kevin Loughead <kvnloughead@gmail.com>
2025-02-23 00:06:51 -0500

    feat: add text_to_textnode function
     - this function converts a markdown string
       to a list of text nodes

---
+++ src/transformers/test_text_to_textnodes.py
@@ -0,0 +1,57 @@
+import unittest
+from transformers.text_to_textnodes import text_to_textnodes
+from nodes.textnode import TextNode, TextType
+
+class TestTextToTextNodes(unittest.TestCase):
+     simple_cases = [
+          {
+               "name": "all types",
+               "text": "Text **bold** and *italic* and `code` and ![an image](image.png) and [a link](https://foo.bar)",
+               "expected": [
+                    TextNode("Text ", TextType.NORMAL),
+                    TextNode("bold", TextType.BOLD),
+                    TextNode(" and ", TextType.NORMAL),
+                    TextNode("italic", TextType.ITALIC),
+                    TextNode(" and ", TextType.NORMAL),
+                    TextNode("code", TextType.CODE),
+                    TextNode(" and ", TextType.NORMAL),
+                    TextNode("an image", TextType.IMAGE, url="image.png"),
+                    TextNode(" and ", TextType.NORMAL),
+                    TextNode("a link", TextType.LINK, url="https://foo.bar"),
+               ]
+          },
+          {
+               "name": "adjacent",
+               "text": "**bold***italic*`code`![an image](image.png)[a link](https://foo.bar)",
+               "expected": [
+                    TextNode("bold", TextType.BOLD),
+                    TextNode("italic", TextType.ITALIC),
+                    TextNode("code", TextType.CODE),
+                    TextNode("an image", TextType.IMAGE, url="image.png"),
+                    TextNode("a link", TextType.LINK, url="https://foo.bar"),
+               ]
+          },
+          {
+              "name": "image nested inside delimiter",
+              "text": "*Here is a [link](https://foo.bar)*",
+              "expected": [TextNode("Here is a ", TextType.NORMAL),
+                           TextNode("link", TextType.LINK, url="https://foo.bar")]
+          },
+          {
+              "name": "only normal text",
+              "text": "Just some normal text",
+              "expected": [TextNode("Just some normal text", TextType.NORMAL)]
+          }
+     ]
+
+     def test_simple_cases(self):
+          for case in self.simple_cases:
+              with self.subTest(case["name"]):
+                  actual = text_to_textnodes(case["text"])
+                  self.assertEqual(actual, case["expected"])
+
+     def test_empty_string(self):
+          with self.assertRaises(Exception) as context:
+              text_to_textnodes("")
+          self.assertTrue('Text must not be empty')
+    
---
+++ src/transformers/text_to_textnodes.py
@@ -0,0 +1,19 @@
+from nodes.textnode import TextNode, TextType
+from transformers.split_nodes_by_delimiter import split_nodes_by_delimiter
+from transformers.split_nodes_by_image import split_nodes_by_image
+from transformers.split_nodes_by_link import split_nodes_by_link
+
+def text_to_textnodes(text):
+    """
+    text_to_textnodes takes a string of markdown and returns a list of textnodes.
+
+    If text is the empty string an exception is raised.
+    """
+    if len(text) == 0:
+        raise Exception("Text must not be empty")
+    with_bold = split_nodes_by_delimiter([TextNode(text, TextType.NORMAL)], "**" ,TextType.BOLD)
+    with_italic = split_nodes_by_delimiter(with_bold, "*", TextType.ITALIC)
+    with_code = split_nodes_by_delimiter(with_italic, "`", TextType.CODE)
+    with_images = split_nodes_by_image(with_code)
+    with_links = split_nodes_by_link(with_images)
+    return with_links
